854611dd0e9f29f4541a5881940c9d213f2f80fe
Marius1155 <mariusgabrielbudai@gmail.com>
2025-07-17 19:13:42 +0300

    uh

--- STARSbackend/settings.py
+++ STARSbackend/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'stars.apps.StarsConfig',
+    'stars',
     'graphene_django',
 ]
 
