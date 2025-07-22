30b66ad8301d672a263bdd9c64f279d48b228030
Marius1155 <mariusgabrielbudai@gmail.com>
2025-07-22 19:38:44 +0300

    making sure i can fetch the schema

--- STARS/urls.py
+++ STARS/urls.py
@@ -4,8 +4,5 @@ from django.urls import path
 
 # stars/urls.py
 from django.urls import path
-from .views import load_data_view
 
-urlpatterns = [
-    path('load-data/', load_data_view),  # TEMPORARY
-]
+urlpatterns = [] # TEMPORARY]
--- STARS/views.py
+++ STARS/views.py
@@ -3,11 +3,3 @@ from django.core.management import call_command
 from django.http import HttpResponse
 from django.conf import settings
 import os
-
-def load_data_view(request):
-    json_path = os.path.join(settings.BASE_DIR, 'data.json')  # adjust filename if needed
-    if os.path.exists(json_path):
-        call_command('loaddata', json_path)
-        return HttpResponse("Data loaded into the database.")
-    else:
-        return HttpResponse("data.json not found at expected path.", status=404)
