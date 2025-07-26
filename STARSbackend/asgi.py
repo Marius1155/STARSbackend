23f9dd4b1fbea4d003eebab86836428072439c27
Marius1155 <mariusgabrielbudai@gmail.com>
2025-07-26 16:16:14 +0300

    On track to make this graphql schema actually work with strawberry#4

--- STARSbackend/asgi.py
+++ STARSbackend/asgi.py
@@ -6,19 +6,25 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
-# STARSbackend/asgi.py
 
 import os
+import django
 from django.core.asgi import get_asgi_application
+
+# --- IMPORTANT ---
+# Set the settings module and run django.setup() BEFORE importing any other
+# components that might need the settings (like your schema or views).
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "STARSbackend.settings")
+django.setup()
+# -----------------
+
+# Now it's safe to import the rest
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path
 from strawberry.asgi import GraphQL
-
-# Corrected import path from your 'STARS' app
 from STARS.graphql.schema import schema
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "STARSbackend.settings")
-
+# This function can now be called safely
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
