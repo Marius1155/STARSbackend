f46cab3df230973eaf37520ddc85571e75835585
Marius1155 <mariusgabrielbudai@gmail.com>
2025-07-26 18:42:40 +0300

    On track to make this graphql schema actually work with strawberry#8

--- STARSbackend/asgi.py
+++ STARSbackend/asgi.py
@@ -1,12 +1,3 @@
-"""
-ASGI config for STARSbackend project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
 # STARSbackend/asgi.py
 
 import os
@@ -16,9 +7,9 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "STARSbackend.settings")
 django.setup()
 
-# Now it's safe to import the rest
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.urls import path
+# Add re_path to the imports
+from django.urls import path, re_path
 from strawberry.asgi import GraphQL
 from STARS.graphql.schema import schema
 
@@ -26,9 +17,10 @@ django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": URLRouter([
-        # Remove the optimizer_enabled argument from this line
         path("graphql/", GraphQL(schema)),
-
-        path("", django_asgi_app),
+        # Use re_path to create a "catch-all" for any other path.
+        # This will correctly hand off requests for static files (or any
+        # other Django URL) to the main Django app.
+        re_path(r"", django_asgi_app),
     ]),
 })
