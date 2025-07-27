da04f42c52b65f796258a68eb8cca112409d35f3
Marius1155 <mariusgabrielbudai@gmail.com>
2025-07-27 15:56:09 +0300

    adding filtering functionality#3

--- STARS/graphql/filters.py
+++ STARS/graphql/filters.py
@@ -34,13 +34,13 @@ class EventFilter:
     location: auto
     is_one_time: auto
     is_featured: auto
-    series_id: auto
+    series: "EventSeriesFilter"  # Allow filtering by the related series
 
 @strawberry_django.filter(models.Review, lookups=True)
 class ReviewFilter:
     id: auto
     stars: auto
-    user_id: auto
+    user: "UserFilter"  # Allow filtering by the related user
     date_created: auto
     is_latest: auto
 
@@ -72,8 +72,8 @@ class SongFilter:
 @strawberry_django.filter(models.SongArtist, lookups=True)
 class SongArtistFilter:
     id: auto
-    song_id: auto
-    artist_id: auto
+    song: "SongFilter"      # CORRECTED: Allow nested filtering on song
+    artist: "ArtistFilter"  # CORRECTED: Allow nested filtering on artist
 
 @strawberry_django.filter(models.Project, lookups=True)
 class ProjectFilter:
@@ -86,14 +86,14 @@ class ProjectFilter:
 @strawberry_django.filter(models.ProjectArtist, lookups=True)
 class ProjectArtistFilter:
     id: auto
-    project_id: auto
-    artist_id: auto
+    project: "ProjectFilter" # CORRECTED: Allow nested filtering on project
+    artist: "ArtistFilter"   # CORRECTED: Allow nested filtering on artist
 
 @strawberry_django.filter(models.ProjectSong, lookups=True)
 class ProjectSongFilter:
     id: auto
-    project_id: auto
-    song_id: auto
+    project: "ProjectFilter" # CORRECTED: Allow nested filtering on project
+    song: "SongFilter"       # CORRECTED: Allow nested filtering on song
 
 @strawberry_django.filter(models.Podcast, lookups=True)
 class PodcastFilter:
@@ -105,7 +105,7 @@ class PodcastFilter:
 @strawberry_django.filter(models.Outfit, lookups=True)
 class OutfitFilter:
     id: auto
-    artist_id: auto
+    artist: "ArtistFilter" # CORRECTED: Allow nested filtering on artist
     date: auto
     is_featured: auto
 
@@ -116,13 +116,13 @@ class ConversationFilter:
 @strawberry_django.filter(models.Message, lookups=True)
 class MessageFilter:
     id: auto
-    sender_id: auto
+    sender: "UserFilter" # CORRECTED: Allow nested filtering on sender
     is_read: auto
 
 @strawberry_django.filter(models.Profile, lookups=True)
 class ProfileFilter:
     id: auto
-    user_id: auto
+    user: "UserFilter" # CORRECTED: Allow nested filtering on user
     has_premium: auto
 
 @strawberry_django.filter(User, lookups=True)
@@ -132,4 +132,3 @@ class UserFilter:
     email: auto
     first_name: auto
     last_name: auto
-
