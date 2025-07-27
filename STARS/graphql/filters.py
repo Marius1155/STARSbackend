a2ffa24450f970e4a0cc3baa21158a598c435267
Marius1155 <mariusgabrielbudai@gmail.com>
2025-07-27 15:42:54 +0300

    adding filtering functionality

---
+++ STARS/graphql/filters.py
@@ -0,0 +1,135 @@
+# STARS/graphql/filters.py
+
+import strawberry
+import strawberry_django
+from strawberry import auto
+
+# Import all the models you want to create filters for
+from STARS import models
+from django.contrib.auth.models import User
+
+# This decorator tells Strawberry to create a GraphQL input type
+# based on the model, which can be used for filtering.
+# The `lookups=True` argument automatically enables advanced
+# filtering options like 'icontains', 'gt' (greater than), etc.
+
+@strawberry_django.filter(models.Artist, lookups=True)
+class ArtistFilter:
+    id: auto
+    name: auto
+    origin: auto
+    is_featured: auto
+
+@strawberry_django.filter(models.EventSeries, lookups=True)
+class EventSeriesFilter:
+    id: auto
+    name: auto
+    is_featured: auto
+
+@strawberry_django.filter(models.Event, lookups=True)
+class EventFilter:
+    id: auto
+    name: auto
+    date: auto
+    location: auto
+    is_one_time: auto
+    is_featured: auto
+    series_id: auto
+
+@strawberry_django.filter(models.Review, lookups=True)
+class ReviewFilter:
+    id: auto
+    stars: auto
+    user_id: auto
+    date_created: auto
+    is_latest: auto
+
+@strawberry_django.filter(models.SubReview, lookups=True)
+class SubReviewFilter:
+    id: auto
+    topic: auto
+    stars: auto
+
+@strawberry_django.filter(models.Cover, lookups=True)
+class CoverFilter:
+    id: auto
+    is_featured: auto
+
+@strawberry_django.filter(models.MusicVideo, lookups=True)
+class MusicVideoFilter:
+    id: auto
+    title: auto
+    release_date: auto
+    is_featured: auto
+
+@strawberry_django.filter(models.Song, lookups=True)
+class SongFilter:
+    id: auto
+    title: auto
+    release_date: auto
+    is_featured: auto
+
+@strawberry_django.filter(models.SongArtist, lookups=True)
+class SongArtistFilter:
+    id: auto
+    song_id: auto
+    artist_id: auto
+
+@strawberry_django.filter(models.Project, lookups=True)
+class ProjectFilter:
+    id: auto
+    title: auto
+    release_date: auto
+    project_type: auto
+    is_featured: auto
+
+@strawberry_django.filter(models.ProjectArtist, lookups=True)
+class ProjectArtistFilter:
+    id: auto
+    project_id: auto
+    artist_id: auto
+
+@strawberry_django.filter(models.ProjectSong, lookups=True)
+class ProjectSongFilter:
+    id: auto
+    project_id: auto
+    song_id: auto
+
+@strawberry_django.filter(models.Podcast, lookups=True)
+class PodcastFilter:
+    id: auto
+    title: auto
+    since: auto
+    is_featured: auto
+
+@strawberry_django.filter(models.Outfit, lookups=True)
+class OutfitFilter:
+    id: auto
+    artist_id: auto
+    date: auto
+    is_featured: auto
+
+@strawberry_django.filter(models.Conversation, lookups=True)
+class ConversationFilter:
+    id: auto
+
+@strawberry_django.filter(models.Message, lookups=True)
+class MessageFilter:
+    id: auto
+    sender_id: auto
+    is_read: auto
+
+@strawberry_django.filter(models.Profile, lookups=True)
+class ProfileFilter:
+    id: auto
+    user_id: auto
+    has_premium: auto
+
+@strawberry_django.filter(User, lookups=True)
+class UserFilter:
+    id: auto
+    username: auto
+    email: auto
+    first_name: auto
+    last_name: auto
+
--- STARS/graphql/schema.py
+++ STARS/graphql/schema.py
@@ -1,28 +1,36 @@
+# STARS/graphql/schema.py
+
 import strawberry
 import strawberry_django
 from strawberry_django.optimizer import DjangoOptimizerExtension
+
+# Import your type and filter definitions
 from . import types
+from . import filters
 
 @strawberry.type
 class Query:
-    artists: list[types.Artist] = strawberry_django.field()
-    projects: list[types.Project] = strawberry_django.field()
-    songs: list[types.Song] = strawberry_django.field()
-    project_songs: list[types.ProjectSong] = strawberry_django.field()
-    project_artists: list[types.ProjectArtist] = strawberry_django.field()
-    song_artists: list[types.SongArtist] = strawberry_django.field()
-    podcasts: list[types.Podcast] = strawberry_django.field()
-    outfits: list[types.Outfit] = strawberry_django.field()
-    reviews: list[types.Review] = strawberry_django.field()
-    sub_reviews: list[types.SubReview] = strawberry_django.field()
-    profiles: list[types.Profile] = strawberry_django.field()
-    messages: list[types.Message] = strawberry_django.field()
-    conversations: list[types.Conversation] = strawberry_django.field()
-    events: list[types.Event] = strawberry_django.field()
-    event_series: list[types.EventSeries] = strawberry_django.field()
-    covers: list[types.Cover] = strawberry_django.field()
-    music_videos: list[types.MusicVideo] = strawberry_django.field()
-    users: list[types.User] = strawberry_django.field()
+    # Add the 'filters' argument to each field, linking it to the
+    # corresponding filter class you defined in filters.py
+    artists: list[types.Artist] = strawberry_django.field(filters=filters.ArtistFilter)
+    projects: list[types.Project] = strawberry_django.field(filters=filters.ProjectFilter)
+    songs: list[types.Song] = strawberry_django.field(filters=filters.SongFilter)
+    project_songs: list[types.ProjectSong] = strawberry_django.field(filters=filters.ProjectSongFilter)
+    project_artists: list[types.ProjectArtist] = strawberry_django.field(filters=filters.ProjectArtistFilter)
+    song_artists: list[types.SongArtist] = strawberry_django.field(filters=filters.SongArtistFilter)
+    podcasts: list[types.Podcast] = strawberry_django.field(filters=filters.PodcastFilter)
+    outfits: list[types.Outfit] = strawberry_django.field(filters=filters.OutfitFilter)
+    reviews: list[types.Review] = strawberry_django.field(filters=filters.ReviewFilter)
+    sub_reviews: list[types.SubReview] = strawberry_django.field(filters=filters.SubReviewFilter)
+    profiles: list[types.Profile] = strawberry_django.field(filters=filters.ProfileFilter)
+    messages: list[types.Message] = strawberry_django.field(filters=filters.MessageFilter)
+    conversations: list[types.Conversation] = strawberry_django.field(filters=filters.ConversationFilter)
+    events: list[types.Event] = strawberry_django.field(filters=filters.EventFilter)
+    event_series: list[types.EventSeries] = strawberry_django.field(filters=filters.EventSeriesFilter)
+    covers: list[types.Cover] = strawberry_django.field(filters=filters.CoverFilter)
+    music_videos: list[types.MusicVideo] = strawberry_django.field(filters=filters.MusicVideoFilter)
+    users: list[types.User] = strawberry_django.field(filters=filters.UserFilter)
+
 
 schema = strawberry.Schema(
     query=Query,
