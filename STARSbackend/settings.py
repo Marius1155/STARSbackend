1f147bf0501884ddfe50111e290e3c1ffa666e69
Marius1155 <mariusgabrielbudai@gmail.com>
2025-07-28 19:01:49 +0300

    added google and apple login#3

--- STARSbackend/settings.py
+++ STARSbackend/settings.py
@@ -27,7 +27,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'dj_rest_auth',
     'rest_framework.authtoken',
     'allauth',
     'allauth.account',
