56c5178dbfdcfd7efd848609c84d3835576ad03d
Marius1155 <mariusgabrielbudai@gmail.com>
2025-08-06 21:06:44 +0300

    created a redis and connected it

--- STARSbackend/settings.py
+++ STARSbackend/settings.py
@@ -40,10 +40,10 @@ INSTALLED_APPS = [
 ]
 
 CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [os.environ.get("REDIS_URL", "redis://localhost:6379")],
         },
     },
 }
