6a105a0bfb7ca4da5c3fad6c90e398fe8ed04a32
Marius1155 <mariusgabrielbudai@gmail.com>
2025-08-08 19:04:13 +0300

    exempted graphql from the csrf token check, cuz apparently thats totally ok and safe

--- STARSbackend/urls.py
+++ STARSbackend/urls.py
@@ -2,15 +2,20 @@
 from django.contrib import admin
 from django.urls import path, include # Make sure 'include' is imported
 from STARS.graphql.schema import schema
+from django.views.decorators.csrf import csrf_exempt
+
 
 def graphql_view():
     # This import is now safe because django.setup() has already run
     from strawberry.django.views import AsyncGraphQLView
-    return AsyncGraphQLView.as_view(schema=schema)
+    return AsyncGraphQLView.as_view(
+        schema=schema,
+        debug=True
+    )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('graphql/', graphql_view()),
+    path('graphql/', csrf_exempt(graphql_view())),
     # Add this line for allauth
     path('accounts/', include('allauth.urls')),
 ]
