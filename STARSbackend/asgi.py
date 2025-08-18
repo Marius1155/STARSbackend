25500ee542ebdba3b32636ad6861b709f77816d5
Marius Gabriel Budai <mariusss@Mariuss-MARVELOUS-MacBook-Pro.local>
2025-08-18 16:47:15 +0300

    fixing the websockets for subscriptions

--- STARSbackend/asgi.py
+++ STARSbackend/asgi.py
@@ -1,14 +1,11 @@
 import os
 from django.core.asgi import get_asgi_application
 
-# Set the settings module BEFORE anything else.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "STARSbackend.settings")
 
-# This call also helps initialize Django.
 django_asgi_app = get_asgi_application()
 
-# Now, import your Channels and Strawberry components.
-from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.routing import ProtocolTypeRouter
 from channels.auth import AuthMiddlewareStack
 from strawberry.channels import GraphQLProtocolTypeRouter
 from STARS.graphql.schema import schema
