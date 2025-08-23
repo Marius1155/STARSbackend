06459189b5dd4695c5994a0a648807773766ac2c
Marius Gabriel Budai <mariusss@Mariuss-MARVELOUS-MacBook-Pro.local>
2025-08-23 15:24:56 +0300

    swithing to relay style queries to see if they also offer me cursor based pagination#3

--- STARS/graphql/schema.py
+++ STARS/graphql/schema.py
@@ -1,26 +1,26 @@
 import strawberry
 import strawberry_django
 from strawberry_django.optimizer import DjangoOptimizerExtension
-from strawberry import relay
+from strawberry_django.relay import DjangoCursorConnection
 
 from . import types, filters, mutations, subscriptions
 
 
 @strawberry.type
 class Query:
-    artists: relay.Connection[types.Artist] = strawberry_django.connection(filters=filters.ArtistFilter)
-    projects: relay.Connection[types.Project] = strawberry_django.connection(filters=filters.ProjectFilter)
-    songs: relay.Connection[types.Song] = strawberry_django.connection(filters=filters.SongFilter)
-    podcasts: relay.Connection[types.Podcast] = strawberry_django.connection(filters=filters.PodcastFilter)
-    outfits: relay.Connection[types.Outfit] = strawberry_django.connection(filters=filters.OutfitFilter)
-    comments: relay.Connection[types.Comment] = strawberry_django.connection(filters=filters.CommentFilter)
-    reviews: relay.Connection[types.Review] = strawberry_django.connection(filters=filters.ReviewFilter)
-    messages: relay.Connection[types.Message] = strawberry_django.connection(filters=filters.MessageFilter)
-    conversations: relay.Connection[types.Conversation] = strawberry_django.connection(filters=filters.ConversationFilter)
-    events: relay.Connection[types.Event] = strawberry_django.connection(filters=filters.EventFilter)
-    event_series: relay.Connection[types.EventSeries] = strawberry_django.connection(filters=filters.EventSeriesFilter)
-    music_videos: relay.Connection[types.MusicVideo] = strawberry_django.connection(filters=filters.MusicVideoFilter)
-    users: relay.Connection[types.User] = strawberry_django.connection(filters=filters.UserFilter)
+    artists: DjangoCursorConnection[types.Artist] = strawberry_django.connection(filters=filters.ArtistFilter)
+    projects: DjangoCursorConnection[types.Project] = strawberry_django.connection(filters=filters.ProjectFilter)
+    songs: DjangoCursorConnection[types.Song] = strawberry_django.connection(filters=filters.SongFilter)
+    podcasts: DjangoCursorConnection[types.Podcast] = strawberry_django.connection(filters=filters.PodcastFilter)
+    outfits: DjangoCursorConnection[types.Outfit] = strawberry_django.connection(filters=filters.OutfitFilter)
+    comments: DjangoCursorConnection[types.Comment] = strawberry_django.connection(filters=filters.CommentFilter)
+    reviews: DjangoCursorConnection[types.Review] = strawberry_django.connection(filters=filters.ReviewFilter)
+    messages: DjangoCursorConnection[types.Message] = strawberry_django.connection(filters=filters.MessageFilter)
+    conversations: DjangoCursorConnection[types.Conversation] = strawberry_django.connection(filters=filters.ConversationFilter)
+    events: DjangoCursorConnection[types.Event] = strawberry_django.connection(filters=filters.EventFilter)
+    event_series: DjangoCursorConnection[types.EventSeries] = strawberry_django.connection(filters=filters.EventSeriesFilter)
+    music_videos: DjangoCursorConnection[types.MusicVideo] = strawberry_django.connection(filters=filters.MusicVideoFilter)
+    users: DjangoCursorConnection[types.User] = strawberry_django.connection(filters=filters.UserFilter)
 
     # These are not paginated
     project_songs: list[types.ProjectSong] = strawberry_django.field(filters=filters.ProjectSongFilter)
@@ -29,7 +29,6 @@ class Query:
     sub_reviews: list[types.SubReview] = strawberry_django.field(filters=filters.SubReviewFilter)
     profiles: list[types.Profile] = strawberry_django.field(filters=filters.ProfileFilter)
     covers: list[types.Cover] = strawberry_django.field(filters=filters.CoverFilter)
-
 
 schema = strawberry.Schema(
     query=Query,
--- STARS/graphql/types.py
+++ STARS/graphql/types.py
@@ -1,8 +1,7 @@
 import strawberry
 import strawberry_django
 from typing import Optional
-from strawberry import relay
-
+from strawberry_django.relay import DjangoCursorConnection
 from STARS import models
 from django.contrib.auth.models import User as DjangoUser
 
@@ -12,22 +11,22 @@ from . import filters
 
 @strawberry_django.type(models.Artist, fields="__all__")
 class Artist(strawberry.relay.Node):
-    song_artists: relay.Connection["SongArtist"] = strawberry_django.connection(filters=filters.SongArtistFilter)
-    project_artists: relay.Connection["ProjectArtist"] = strawberry_django.connection(filters=filters.ProjectArtistFilter)
-    outfits: relay.Connection["Outfit"] = strawberry_django.connection(filters=filters.OutfitFilter)
-    podcasts: relay.Connection["Podcast"] = strawberry_django.connection(filters=filters.PodcastFilter)
+    song_artists: DjangoCursorConnection["SongArtist"] = strawberry_django.connection(filters=filters.SongArtistFilter)
+    project_artists: DjangoCursorConnection["ProjectArtist"] = strawberry_django.connection(filters=filters.ProjectArtistFilter)
+    outfits: DjangoCursorConnection["Outfit"] = strawberry_django.connection(filters=filters.OutfitFilter)
+    podcasts: DjangoCursorConnection["Podcast"] = strawberry_django.connection(filters=filters.PodcastFilter)
 
 
 @strawberry_django.type(models.EventSeries, fields="__all__")
 class EventSeries(strawberry.relay.Node):
-    events: relay.Connection["Event"] = strawberry_django.connection(filters=filters.EventFilter)
+    events: DjangoCursorConnection["Event"] = strawberry_django.connection(filters=filters.EventFilter)
 
 
 @strawberry_django.type(models.Event, fields="__all__")
 class Event(strawberry.relay.Node):
     series: "EventSeries"
-    reviews: relay.Connection["Review"] = strawberry_django.connection(filters=filters.ReviewFilter)
-    outfits: relay.Connection["Outfit"] = strawberry_django.connection(filters=filters.OutfitFilter)
+    reviews: DjangoCursorConnection["Review"] = strawberry_django.connection(filters=filters.ReviewFilter)
+    outfits: DjangoCursorConnection["Outfit"] = strawberry_django.connection(filters=filters.OutfitFilter)
 
 
 @strawberry_django.type(
@@ -36,25 +35,25 @@ class Event(strawberry.relay.Node):
 )
 class User(strawberry.relay.Node):
     profile: "Profile"
-    conversations: relay.Connection["Conversation"] = strawberry_django.connection(filters=filters.ConversationFilter)
-    reviews: relay.Connection["Review"] = strawberry_django.connection(filters=filters.ReviewFilter)
+    conversations: DjangoCursorConnection["Conversation"] = strawberry_django.connection(filters=filters.ConversationFilter)
+    reviews: DjangoCursorConnection["Review"] = strawberry_django.connection(filters=filters.ReviewFilter)
 
 
 @strawberry_django.type(models.Comment, fields="__all__")
 class Comment(strawberry.relay.Node):
     review: "Review"
     user: "User"
-    liked_by: relay.Connection["User"] = strawberry_django.connection(filters=filters.UserFilter)
-    disliked_by: relay.Connection["User"] = strawberry_django.connection(filters=filters.UserFilter)
+    liked_by: DjangoCursorConnection["User"] = strawberry_django.connection(filters=filters.UserFilter)
+    disliked_by: DjangoCursorConnection["User"] = strawberry_django.connection(filters=filters.UserFilter)
 
 
 @strawberry_django.type(models.Review, fields="__all__")
 class Review(strawberry.relay.Node):
     user: "User"
     content_object: "Reviewable"
-    subreviews: relay.Connection["SubReview"] = strawberry_django.connection(filters=filters.SubReviewFilter)
-    liked_by: relay.Connection["User"] = strawberry_django.connection(filters=filters.UserFilter)
-    disliked_by: relay.Connection["User"] = strawberry_django.connection(filters=filters.UserFilter)
+    subreviews: DjangoCursorConnection["SubReview"] = strawberry_django.connection(filters=filters.SubReviewFilter)
+    liked_by: DjangoCursorConnection["User"] = strawberry_django.connection(filters=filters.UserFilter)
+    disliked_by: DjangoCursorConnection["User"] = strawberry_django.connection(filters=filters.UserFilter)
 
 
 @strawberry_django.type(models.SubReview, fields="__all__")
@@ -65,23 +64,23 @@ class SubReview(strawberry.relay.Node):
 @strawberry_django.type(models.Cover, fields="__all__")
 class Cover(strawberry.relay.Node):
     content_object: "Coverable"
-    reviews: relay.Connection["Review"] = strawberry_django.connection(filters=filters.ReviewFilter)
+    reviews: DjangoCursorConnection["Review"] = strawberry_django.connection(filters=filters.ReviewFilter)
 
 
 @strawberry_django.type(models.MusicVideo, fields="__all__")
 class MusicVideo(strawberry.relay.Node):
-    songs: relay.Connection["Song"] = strawberry_django.connection(filters=filters.SongFilter)
-    reviews: relay.Connection["Review"] = strawberry_django.connection(filters=filters.ReviewFilter)
-    outfits: relay.Connection["Outfit"] = strawberry_django.connection(filters=filters.OutfitFilter)
+    songs: DjangoCursorConnection["Song"] = strawberry_django.connection(filters=filters.SongFilter)
+    reviews: DjangoCursorConnection["Review"] = strawberry_django.connection(filters=filters.ReviewFilter)
+    outfits: DjangoCursorConnection["Outfit"] = strawberry_django.connection(filters=filters.OutfitFilter)
 
 
 @strawberry_django.type(models.Song, fields="__all__")
 class Song(strawberry.relay.Node):
-    alternative_versions: relay.Connection["Song"] = strawberry_django.connection(filters=filters.SongFilter)
-    song_artists: relay.Connection["SongArtist"] = strawberry_django.connection(filters=filters.SongArtistFilter)
-    project_songs: relay.Connection["ProjectSong"] = strawberry_django.connection(filters=filters.ProjectSongFilter)
-    music_videos: relay.Connection["MusicVideo"] = strawberry_django.connection(filters=filters.MusicVideoFilter)
-    reviews: relay.Connection["Review"] = strawberry_django.connection(filters=filters.ReviewFilter)
+    alternative_versions: DjangoCursorConnection["Song"] = strawberry_django.connection(filters=filters.SongFilter)
+    song_artists: DjangoCursorConnection["SongArtist"] = strawberry_django.connection(filters=filters.SongArtistFilter)
+    project_songs: DjangoCursorConnection["ProjectSong"] = strawberry_django.connection(filters=filters.ProjectSongFilter)
+    music_videos: DjangoCursorConnection["MusicVideo"] = strawberry_django.connection(filters=filters.MusicVideoFilter)
+    reviews: DjangoCursorConnection["Review"] = strawberry_django.connection(filters=filters.ReviewFilter)
 
 
 @strawberry_django.type(models.SongArtist, fields="__all__")
@@ -92,11 +91,11 @@ class SongArtist(strawberry.relay.Node):
 
 @strawberry_django.type(models.Project, fields="__all__")
 class Project(strawberry.relay.Node):
-    covers: relay.Connection["Cover"] = strawberry_django.connection(filters=filters.CoverFilter)
-    alternative_versions: relay.Connection["Project"] = strawberry_django.connection(filters=filters.ProjectFilter)
-    project_songs: relay.Connection["ProjectSong"] = strawberry_django.connection(filters=filters.ProjectSongFilter)
-    project_artists: relay.Connection["ProjectArtist"] = strawberry_django.connection(filters=filters.ProjectArtistFilter)
-    reviews: relay.Connection["Review"] = strawberry_django.connection(filters=filters.ReviewFilter)
+    covers: DjangoCursorConnection["Cover"] = strawberry_django.connection(filters=filters.CoverFilter)
+    alternative_versions: DjangoCursorConnection["Project"] = strawberry_django.connection(filters=filters.ProjectFilter)
+    project_songs: DjangoCursorConnection["ProjectSong"] = strawberry_django.connection(filters=filters.ProjectSongFilter)
+    project_artists: DjangoCursorConnection["ProjectArtist"] = strawberry_django.connection(filters=filters.ProjectArtistFilter)
+    reviews: DjangoCursorConnection["Review"] = strawberry_django.connection(filters=filters.ReviewFilter)
 
 
 @strawberry_django.type(models.ProjectArtist, fields="__all__")
@@ -113,27 +112,27 @@ class ProjectSong(strawberry.relay.Node):
 
 @strawberry_django.type(models.Podcast, fields="__all__")
 class Podcast(strawberry.relay.Node):
-    hosts: relay.Connection["Artist"] = strawberry_django.connection(filters=filters.ArtistFilter)
-    covers: relay.Connection["Cover"] = strawberry_django.connection(filters=filters.CoverFilter)
-    reviews: relay.Connection["Review"] = strawberry_django.connection(filters=filters.ReviewFilter)
+    hosts: DjangoCursorConnection["Artist"] = strawberry_django.connection(filters=filters.ArtistFilter)
+    covers: DjangoCursorConnection["Cover"] = strawberry_django.connection(filters=filters.CoverFilter)
+    reviews: DjangoCursorConnection["Review"] = strawberry_django.connection(filters=filters.ReviewFilter)
 
 
 @strawberry_django.type(models.Outfit, fields="__all__")
 class Outfit(strawberry.relay.Node):
     artist: "Artist"
-    music_videos: relay.Connection["MusicVideo"] = strawberry_django.connection(filters=filters.MusicVideoFilter)
-    covers: relay.Connection["Cover"] = strawberry_django.connection(filters=filters.CoverFilter)
-    matches: relay.Connection["Outfit"] = strawberry_django.connection(filters=filters.OutfitFilter)
-    events: relay.Connection["Event"] = strawberry_django.connection(filters=filters.EventFilter)
-    reviews: relay.Connection["Review"] = strawberry_django.connection(filters=filters.ReviewFilter)
+    music_videos: DjangoCursorConnection["MusicVideo"] = strawberry_django.connection(filters=filters.MusicVideoFilter)
+    covers: DjangoCursorConnection["Cover"] = strawberry_django.connection(filters=filters.CoverFilter)
+    matches: DjangoCursorConnection["Outfit"] = strawberry_django.connection(filters=filters.OutfitFilter)
+    events: DjangoCursorConnection["Event"] = strawberry_django.connection(filters=filters.EventFilter)
+    reviews: DjangoCursorConnection["Review"] = strawberry_django.connection(filters=filters.ReviewFilter)
 
 
 @strawberry_django.type(models.Conversation, fields="__all__")
 class Conversation(strawberry.relay.Node):
     latest_message: Optional["Message"]
     latest_message_sender: Optional["User"]
-    participants: relay.Connection["User"] = strawberry_django.connection(filters=filters.UserFilter)
-    messages: relay.Connection["Message"] = strawberry_django.connection(filters=filters.MessageFilter)
+    participants: DjangoCursorConnection["User"] = strawberry_django.connection(filters=filters.UserFilter)
+    messages: DjangoCursorConnection["Message"] = strawberry_django.connection(filters=filters.MessageFilter)
 
 
 @strawberry_django.type(models.Message, fields="__all__")
@@ -141,14 +140,14 @@ class Message(strawberry.relay.Node):
     conversation: "Conversation"
     sender: Optional["User"]
     replying_to: Optional["Message"]
-    liked_by: relay.Connection["User"] = strawberry_django.connection(filters=filters.UserFilter)
+    liked_by: DjangoCursorConnection["User"] = strawberry_django.connection(filters=filters.UserFilter)
 
 
 @strawberry_django.type(models.Profile, fields="__all__")
 class Profile(strawberry.relay.Node):
     user: "User"
-    followers: relay.Connection["Profile"] = strawberry_django.connection(filters=filters.ProfileFilter)
-    following: relay.Connection["Profile"] = strawberry_django.connection(filters=filters.ProfileFilter)
+    followers: DjangoCursorConnection["Profile"] = strawberry_django.connection(filters=filters.ProfileFilter)
+    following: DjangoCursorConnection["Profile"] = strawberry_django.connection(filters=filters.ProfileFilter)
 
 
 # The Unions still work correctly with these simplified types
