f235b58aa54a63efc6cde531077bea8c7fdd69ec
Marius Gabriel Budai <mariusss@Mariuss-MARVELOUS-MacBook-Pro.local>
2026-02-18 15:59:10 +0200

    fixed some stuff

--- STARS/graphql/subscriptions.py
+++ STARS/graphql/subscriptions.py
@@ -40,7 +40,7 @@ class ConversationEventPayload:
 @strawberry.type
 class Subscription:
     @strawberry.subscription
-    async def message_events(self, info, conversation_id: int) -> AsyncGenerator[MessageEventUnion, None]:
+    async def message_events(self, info: strawberry.Info, conversation_id: int) -> AsyncGenerator[MessageEventUnion, None]:
         # TEMPORARY: fallback user
         user = info.context.get("user")
         if not user or not user.is_authenticated:
@@ -81,7 +81,7 @@ class Subscription:
             await channel_layer.group_discard(group_name, channel_name)
 
     @strawberry.subscription
-    async def conversation_updates(self, info) -> AsyncGenerator[ConversationEventPayload, None]:
+    async def conversation_updates(self, info: strawberry.Info) -> AsyncGenerator[ConversationEventPayload, None]:
         user = info.context.get("user")
 
         if not user or not user.is_authenticated:
@@ -129,25 +129,6 @@ class Subscription:
 # -----------------------------------------------------------------------------
 # Signal Handlers & Broadcast Functions
 # -----------------------------------------------------------------------------
-"""
-@receiver(post_save, sender=models.Message)
-def handle_message_save(sender, instance, created, update_fields, **kwargs):
-    if created:
-        async_to_sync(broadcast_message_event)(instance, "created")
-    elif update_fields and ('is_read' in update_fields or 'is_delivered' in update_fields):
-        async_to_sync(broadcast_message_event)(instance, "updated")
-
-
-@receiver(m2m_changed, sender=models.Message.liked_by.through)
-def handle_message_like(sender, instance, action, **kwargs):
-    if action in ["post_add", "post_remove"]:
-        async_to_sync(broadcast_message_event)(instance, "updated")
-
-
-@receiver(post_delete, sender=models.Message)
-def handle_message_delete(sender, instance, **kwargs):
-    async_to_sync(broadcast_message_event)(instance, "deleted")
-"""
 
 async def broadcast_message_event(message_id: strawberry.ID, conversation_id: strawberry.ID, event_type: str):
     channel_layer = get_channel_layer()
@@ -160,13 +141,6 @@ async def broadcast_message_event(message_id: strawberry.ID, conversation_id: st
         group_name,
         {"type": "subscription.event", "data": await database_sync_to_async(create_payload)()},
     )
-
-"""
-@receiver(post_save, sender=models.Conversation)
-def handle_conversation_save(sender, instance, created, update_fields, **kwargs):
-    if not created and update_fields and 'latest_message' in update_fields:
-        async_to_sync(broadcast_conversation_update)(instance)
-"""
 
 # In subscriptions.py
 async def broadcast_conversation_update(conversation_id: int):
@@ -182,9 +156,9 @@ async def broadcast_conversation_update(conversation_id: int):
     participant_ids = await database_sync_to_async(get_participant_ids)()
 
     for user_id in participant_ids:
-        print("smth is happening")
-        print(user_id)
-        print("done")
+        # print("smth is happening")
+        # print(user_id)
+        # print("done")
         group_name = f"user_{user_id}_conversations"
         await channel_layer.group_send(
             group_name,
